b0c1a2983c3946238999963eff18474bcd8b4edd
Valentine Rybin <valentine.rybin@gmail.com>
2015-11-22 13:23:22 +0300

    Deleted obsolete scripts

--- emptyFoldersFinder.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import os
-import filesystemanalyse
-with open('emptyfolderslist.txt', 'wt', encoding="utf8") as file:
-    for dirname in filesystemanalyse.findEmptyFolders():
-            file.write(dirname + '\n')
