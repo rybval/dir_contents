a6eeba81b3d726aa77b9b7fd5212f8e51ce4c0d8
Valentine Rybin <valentine.rybin@gmail.com>
2015-11-25 16:15:13 +0300

    Changed tree.py

--- lib/tree.py
+++ lib/tree.py
@@ -2,23 +2,52 @@ import os
 import sys
 
 class Item:
-    def _normalisePath(path_string):
+    def _normCase(path_string):
         if sys.platform == 'win32':
             return path_string.casefold()
         else:
             return path_string
-     
-    def getSize(self):
-        return __size
+
+    def getSize(self):  return self.__size
+        
+    def getName(self):  return self.__name
+        
+    def getPath(self):
+        return os.path.join(self.__parent.getPath(), self.__name())
     
-    def __init__(self, name):
+    def __init__(self, name, parent):
         self.__name = name
-        
-class Directory:        
-    def __init__(self, path, name=None, parent=None):
-        
-        
+        self.__parent = parent
+        self.__size = None
+
+
 class File(Item):
-    def __init__(self, name, parent=None):
+    def getPath
+    def __init__(self, name, parent):
+        Item.__init__(self, name, parent)
+        self.__hash = None
+        self.__inode = None
+        os.stat(os.path)
+
+
+class Dir(Item):        
+    def __init__(self, name, parent, skeleton = False):
+        Item.__init__(self, name, parent)
+        self.__children = []
         
-        Item.__init__(self, name)
+
+class Root(Directory):
+    def _normPath(path):
+        while path.endswith(os.sep):
+            path = path[:-1]
+        return path
+        
+    def getPath(self):
+        return os.path.join(self.__parentdir, self.__name())
+ 
+    def __init__(self, path, skeleton = False):
+        path = self._normPath(path)
+        self.__parentdir = path.dirname()
+        Directory.__init__(self, path.basename(), None, skeleton)
+
+        
