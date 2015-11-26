d72a3304d8d85f4862a75ad0f13b86cbeee681c9
Valentine Rybin <valentine.rybin@gmail.com>
2015-11-26 22:43:39 +0300

    Changed tree.py

--- lib/tree.py
+++ lib/tree.py
@@ -7,9 +7,13 @@ class Item:
             return path_string.casefold()
         else:
             return path_string
-
-    def getSize(self):  return self.__size
+    def findSize(self): pass
+    def getSize(self):
+        if self.__size == None:
+            self.findSize()        
+        return self.__size
     def getName(self):  return self.__name
+    def getHash(self):  return self.__hash
     def getPath(self):
         return os.path.join(self.__parent.getPath(), self.__name())
     
@@ -17,26 +21,37 @@ class Item:
         self.__name = name
         self.__parent = parent
         self.__size = None
+        self.__hash = None
 
 
 class File(Item):
-    def getHash(self):  return self.__hash
+    def findSize(self):
+        self.__size = os.path.getsize(self.getPath())
+        
     def getInode(self): return self.__inode
-    
+    def getDevice(self): return self.__device
+    def getHardlinks(self): return self.__hlinks
     def __init__(self, name, parent):
-        Item.__init__(self, name, parent)
-        self.__hash = None
+        Item.__init__(self, name, parent, find_size, find_inode, find_hash)
         self.__inode = None
+        self.__device = None
+        self.__hlinks = None
         os.stat(os.path)
 
 
 class Dir(Item):
+    def findSize(self):
+        size = 0
+        for item in self.__content:
+            size += item.getSize()
+        self.__size = size
+        
     def getContent(self): return self.__content
     def getFiles(self):
         return tuple(i for i in self.__content if type(i) is File) 
     def getDirs(self):
         return tuple(i for i in self.__content if type(i) is Dir)
-    def __init__(self, name, parent, skeleton = False):
+    def __init__(self, name, parent, find_sizes, find_inodes, find_hashes):
         Item.__init__(self, name, parent)
         self.__content = tuple()
         
@@ -50,9 +65,12 @@ class Root(Directory):
     def getPath(self):
         return os.path.join(self.__parentdir, self.__name())
  
-    def __init__(self, path, skeleton = False):
+    def __init__(self, path, find_sizes=False, 
+                 find_inodes=False, find_hashes=False):
         path = self._normPath(path)
         self.__parentdir = path.dirname()
-        Directory.__init__(self, path.basename(), None, skeleton)
+        Dir.__init__(self, path.basename(), None,
+                           find_sizes, find_inodes, find_hashes)
+        
 
         
